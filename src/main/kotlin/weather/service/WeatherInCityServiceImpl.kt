6f0589a880c436ae6a6d00d7f48f33c6ad8fd70b
vladinzki <vlad.slinchuk@gmail.com>
2023-01-27 20:52:07 +0200

    Add responsibility to save data weather in db, and get it from if exists

--- src/main/kotlin/weather/service/WeatherInCityServiceImpl.kt
+++ src/main/kotlin/weather/service/WeatherInCityServiceImpl.kt
@@ -6,21 +6,18 @@ import weather.dto.WeatherDto
 import weather.entity.MainInfoEntity
 import weather.entity.WeatherEntity
 import weather.remote.WeatherRemote
-import weather.repository.MainInfoRepository
 import weather.repository.WeatherRepository
 
 @Service
 class WeatherInCityServiceImpl(
     private val weatherRepository: WeatherRepository,
-    private val mainInfoRepository: MainInfoRepository,
     private val remote: WeatherRemote
 ) : WeatherInCityService {
 
     override fun getWeather(cityName: String): WeatherDto {
-        val exsitingWeather = weatherRepository.getByName(cityName)
+        return weatherRepository.getByName(cityName)
             ?.toDto()
-            ?:remote.getWeather(cityName)
-        return exsitingWeather
+            ?: remote.getWeather(cityName)
     }
 
     private fun WeatherEntity.toDto(): WeatherDto =
