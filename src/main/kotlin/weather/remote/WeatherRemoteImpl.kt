05184c304338a5fd1a15977dc3658a4603e4171b
vladinzki <vlad.slinchuk@gmail.com>
2023-01-30 04:28:10 +0200

    some fixes

--- src/main/kotlin/weather/remote/WeatherRemoteImpl.kt
+++ src/main/kotlin/weather/remote/WeatherRemoteImpl.kt
@@ -1,6 +1,5 @@
 package weather.remote
 
-import org.springframework.http.converter.json.KotlinSerializationJsonHttpMessageConverter
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.getForObject
@@ -13,36 +12,13 @@ import weather.repository.WeatherRepository
 
 
 @Component
-class WeatherRemoteImpl(
-    private val weatherRepository: WeatherRepository,
-    private val mainInfoRepository: MainInfoRepository
-) : WeatherRemote {
+class WeatherRemoteImpl : WeatherRemote {
+
     override fun getWeather(cityName: String): WeatherDto {
         val uri =
             "https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=01beaeaff35006a35f03b2073622e429&units=metric"
         val client = RestTemplate()
-        val response = client.getForObject<WeatherDto>(uri)
-        val weatherEntity = weatherRepository.save(response.toEntity())
-        mainInfoRepository.save(response.main.toEntity(weatherEntity))
 
-        return response
+        return client.getForObject(uri)
     }
-
-
-    private fun WeatherDto.toEntity(): WeatherEntity =
-        WeatherEntity(
-            id = this.id,
-            name = this.name,
-            dt = this.dt
-        )
-
-    private fun MainInfoDto.toEntity(weatherEntity: WeatherEntity): MainInfoEntity =
-        MainInfoEntity(
-            id = 0,
-            temp = this.temp,
-            feels_like = this.feels_like,
-            weather = weatherEntity
-        )
-
-
 }
--- src/main/kotlin/weather/service/WeatherInCityServiceImpl.kt
+++ src/main/kotlin/weather/service/WeatherInCityServiceImpl.kt
@@ -1,23 +1,38 @@
 package weather.service
 
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import weather.dto.MainInfoDto
 import weather.dto.WeatherDto
 import weather.entity.MainInfoEntity
 import weather.entity.WeatherEntity
 import weather.remote.WeatherRemote
+import weather.repository.MainInfoRepository
 import weather.repository.WeatherRepository
 
 @Service
 class WeatherInCityServiceImpl(
     private val weatherRepository: WeatherRepository,
-    private val remote: WeatherRemote
+    private val remote: WeatherRemote,
+    private val mainInfoRepository: MainInfoRepository
 ) : WeatherInCityService {
 
+    @Transactional
     override fun getWeather(cityName: String): WeatherDto {
-        return weatherRepository.getByName(cityName)
-            ?.toDto()
-            ?: remote.getWeather(cityName)
+        val response = remote.getWeather(cityName)
+        var existingWeather = weatherRepository.getByName(cityName)
+        if (existingWeather != null) {
+            if (existingWeather.dt != response.dt) {
+                existingWeather.dt = response.dt
+                existingWeather = weatherRepository.save(existingWeather)
+                mainInfoRepository.deleteAllByWeather(existingWeather)
+                mainInfoRepository.save(response.main.toEntity(existingWeather))
+                return existingWeather.toDto()
+            }
+        }
+        val weatherEntity = weatherRepository.save(response.toEntity())
+        mainInfoRepository.save(response.main.toEntity(weatherEntity))
+        return response
     }
 
     private fun WeatherEntity.toDto(): WeatherDto =
@@ -32,6 +47,21 @@ class WeatherInCityServiceImpl(
         MainInfoDto(
             temp = this.temp,
             feels_like = this.feels_like
+        )
+
+    private fun WeatherDto.toEntity(): WeatherEntity =
+        WeatherEntity(
+            id = this.id,
+            name = this.name,
+            dt = this.dt
+        )
+
+    private fun MainInfoDto.toEntity(weatherEntity: WeatherEntity): MainInfoEntity =
+        MainInfoEntity(
+            id = 0,
+            temp = this.temp,
+            feels_like = this.feels_like,
+            weather = weatherEntity
         )
 }
 
